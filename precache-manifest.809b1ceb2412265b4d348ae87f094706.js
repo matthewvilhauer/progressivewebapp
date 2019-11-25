759f015711f4aaf88ac21b98eefa6ea2e863eb0d
Matthew Vilhauer <matthewvilhauer@gmail.com>
2019-11-25 16:09:50 -0500

    Updates

---
+++ precache-manifest.809b1ceb2412265b4d348ae87f094706.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f776412bd6036b55da0dbcc8d35a0ab9",
+    "url": "/progressivewebappfavicon.ico"
+  },
+  {
+    "revision": "c5ea9ab06583a0da47831f6c4631fc07",
+    "url": "/progressivewebappindex.html"
+  },
+  {
+    "url": "/progressivewebappjs/0.bundle.6348eeda115ca2fdd3db.min.js"
+  },
+  {
+    "url": "/progressivewebappjs/0.bundle.6348eeda115ca2fdd3db.min.js.map"
+  },
+  {
+    "url": "/progressivewebappjs/2.bundle.6348eeda115ca2fdd3db.min.js"
+  },
+  {
+    "url": "/progressivewebappjs/2.bundle.6348eeda115ca2fdd3db.min.js.map"
+  },
+  {
+    "url": "/progressivewebappjs/bundle.6348eeda115ca2fdd3db.min.js"
+  },
+  {
+    "url": "/progressivewebappjs/bundle.6348eeda115ca2fdd3db.min.js.map"
+  },
+  {
+    "url": "/progressivewebappmanifest.df46a0cdd1b5c392ad7896ebf48c6ac8.json"
+  }
+]);
